1b631cd816ddacd4fbdff8ff310571b5ac74c030
Abazr Amin <135216609+abazramin@users.noreply.github.com>
2025-02-22 13:03:57 +0300

    Update clsDataAccessSettings.cs

--- DVLD_DataAccess/clsDataAccessSettings.cs
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -4,7 +4,7 @@ namespace DVLD_DataAccess
 {
     static class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=123456;";
+        public static string ConnectionString = "Write Your Code";
 
 
     }
